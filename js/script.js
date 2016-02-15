6a8ebefd925e2dde9329e0de892ef99de8bd4ca5
Jay Mo <jayhxmo@gmail.com>
2016-02-15 13:05:11 -0500

    Type effect on placeholder

--- js/script.js
+++ js/script.js
@@ -1,90 +1,88 @@
+$(function() {
+    $('#search-box').typed({
+        strings: ["anything", "people", "places", "news", "pictures", "apps", "websites", "events", "products", "flights", "things", "cat videos"],
+        loop: true,
+        typeSpeed: 25,
+        backSpeed: 25,
+        backDelay: 1000,
+        shuffle: true,
+        attr: 'placeholder'
+    });
+});
+
+
 urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
-placeholders = ["anything", "people", "places", "websites", "news"];
 currentPlaceholder = 0;
 showSuggestions = false;
 
 function suggest(data) {
-	if (showSuggestions) {
-		if ($('#suggestions').css('display') === 'none') {
-			$('#suggestions').css('opacity', 1);
-		}
+    if (showSuggestions) {
+        if ($('#suggestions').css('display') === 'none') {
+            $('#suggestions').css('opacity', 1);
+        }
 
-		console.log(data);
-		var suggestLength = data[1].length;
-		if (suggestLength > 5) suggestLength = 5;
+        console.log(data);
+        var suggestLength = data[1].length;
+        if (suggestLength > 5) suggestLength = 5;
 
 
-		$('#suggestions').html("");
-		for (var i = 1; i <= suggestLength; i++) {
-			$('#suggestions').append("<span>" + data[1][i][0] + "</span>");
-		}
-	}
+        $('#suggestions').html("");
+        for (var i = 1; i <= suggestLength; i++) {
+            $('#suggestions').append("<span>" + data[1][i][0] + "</span>");
+        }
+    }
 }
 
-var suggestTimeout = setTimeout(function(){}, 500);
+var suggestTimeout = setTimeout(function() {}, 500);
 
-$('#search-box').on('keyup', function(e){
-	if (e.keyCode === 40){
-		$('#search-box').val($('#suggestions').text());
-		$('#suggestions').css('opacity', 0);
-	}else{
-		clearTimeout(suggestTimeout);
+$('#search-box').on('keyup', function(e) {
+    if (e.keyCode === 40) {
+        $('#search-box').val($('#suggestions').text());
+        $('#suggestions').css('opacity', 0);
+    } else {
+        clearTimeout(suggestTimeout);
 
-		var query = $('#search-box').val();
+        var query = $('#search-box').val();
 
-		if(query.match(urlRegex) && query.indexOf("?") !== 0){
-			$('#search').addClass('url');
-		}else{
-			$('#search').removeClass('url');
-		}
+        if (query.match(urlRegex) && query.indexOf("?") !== 0) {
+            $('#search').addClass('url');
+        } else {
+            $('#search').removeClass('url');
+        }
 
-		if (query === "") {
-			$('#suggestions').css('opacity', 0);
-			showSuggestions = false;
-		}
+        if (query === "") {
+            $('#suggestions').css('opacity', 0);
+            showSuggestions = false;
+        } else {
+            showSuggestions = true;
+            $('#suggestions').css('opacity', 1);
 
-		else {
-			showSuggestions = true;
-			$('#suggestions').css('opacity', 1);
-
-			suggestTimeout = setTimeout(function(){
-				$.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
-		            {
-		              "hl":"en", // Language
-		              "jsonp":"suggest", // jsonp callback function name
-		              "q":query, // query term
-		              "client":"youtube" // force youtube style response, i.e. jsonp
-		            }
-		    	);
-			}, 100);
-		}
-	}
+            suggestTimeout = setTimeout(function() {
+                $.getJSON("http://suggestqueries.google.com/complete/search?callback=?", {
+                    "hl": "en", // Language
+                    "jsonp": "suggest", // jsonp callback function name
+                    "q": query, // query term
+                    "client": "youtube" // force youtube style response, i.e. jsonp
+                });
+            }, 100);
+        }
+    }
 });
 
-$('#search').submit(function(e){
-	$('#search').fadeOut(function(){
-		var query = $('#search-box').val();
+$('#search').submit(function(e) {
+    $('#search').fadeOut(function() {
+        var query = $('#search-box').val();
 
-		if (query.match(urlRegex) && query.indexOf("?") !== 0){
-			window.location = query;
-			return false;
-		}
+        if (query.match(urlRegex) && query.indexOf("?") !== 0) {
+            window.location = query;
+            return false;
+        }
 
-		if (query.indexOf("?") === 0) {
-			query = query.substring(1);
-		}
+        if (query.indexOf("?") === 0) {
+            query = query.substring(1);
+        }
 
-		window.location = "https://google.com/#q=" + encodeURIComponent(query);
-	});
-	return false;
+        window.location = "https://google.com/#q=" + encodeURIComponent(query);
+    });
+    return false;
 });
-
-// setInterval(function(){
-// 	if((placeholders[currentPlaceholder+1]) === undefined){
-// 		$('#search-box').attr('placeholder', 'Search for ' + placeholders[0] + '...');
-// 		currentPlaceholder = 0;
-// 	}else{
-// 		currentPlaceholder++;
-// 		$('#search-box').attr('placeholder', 'Search for ' + placeholders[currentPlaceholder] + '...');
-// 	}
-// }, 5000);
