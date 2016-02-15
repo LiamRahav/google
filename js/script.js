62649a568de4b325b37108a62efbd75f276477d3
Jay Mo <jayhxmo@gmail.com>
2016-02-15 12:26:08 -0500

    Beautiful suggestions :')

---
+++ js/script.js
@@ -0,0 +1,90 @@
+urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+placeholders = ["anything", "people", "places", "websites", "news"];
+currentPlaceholder = 0;
+showSuggestions = false;
+
+function suggest(data) {
+	if (showSuggestions) {
+		if ($('#suggestions').css('display') === 'none') {
+			$('#suggestions').css('opacity', 1);
+		}
+
+		console.log(data);
+		var suggestLength = data[1].length;
+		if (suggestLength > 5) suggestLength = 5;
+
+
+		$('#suggestions').html("");
+		for (var i = 1; i <= suggestLength; i++) {
+			$('#suggestions').append("<span>" + data[1][i][0] + "</span>");
+		}
+	}
+}
+
+var suggestTimeout = setTimeout(function(){}, 500);
+
+$('#search-box').on('keyup', function(e){
+	if (e.keyCode === 40){
+		$('#search-box').val($('#suggestions').text());
+		$('#suggestions').css('opacity', 0);
+	}else{
+		clearTimeout(suggestTimeout);
+
+		var query = $('#search-box').val();
+
+		if(query.match(urlRegex) && query.indexOf("?") !== 0){
+			$('#search').addClass('url');
+		}else{
+			$('#search').removeClass('url');
+		}
+
+		if (query === "") {
+			$('#suggestions').css('opacity', 0);
+			showSuggestions = false;
+		}
+
+		else {
+			showSuggestions = true;
+			$('#suggestions').css('opacity', 1);
+
+			suggestTimeout = setTimeout(function(){
+				$.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+		            {
+		              "hl":"en", // Language
+		              "jsonp":"suggest", // jsonp callback function name
+		              "q":query, // query term
+		              "client":"youtube" // force youtube style response, i.e. jsonp
+		            }
+		    	);
+			}, 100);
+		}
+	}
+});
+
+$('#search').submit(function(e){
+	$('#search').fadeOut(function(){
+		var query = $('#search-box').val();
+
+		if (query.match(urlRegex) && query.indexOf("?") !== 0){
+			window.location = query;
+			return false;
+		}
+
+		if (query.indexOf("?") === 0) {
+			query = query.substring(1);
+		}
+
+		window.location = "https://google.com/#q=" + encodeURIComponent(query);
+	});
+	return false;
+});
+
+// setInterval(function(){
+// 	if((placeholders[currentPlaceholder+1]) === undefined){
+// 		$('#search-box').attr('placeholder', 'Search for ' + placeholders[0] + '...');
+// 		currentPlaceholder = 0;
+// 	}else{
+// 		currentPlaceholder++;
+// 		$('#search-box').attr('placeholder', 'Search for ' + placeholders[currentPlaceholder] + '...');
+// 	}
+// }, 5000);
